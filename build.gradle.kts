55fc03f60075f16cdf5a4e29189dc5d435907a82
Philipp Heuer <git@philippheuer.me>
2023-07-09 23:45:11 +0200

    fix: add kotlin plugin directly

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("me.philippheuer.configuration") version "0.10.6"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
